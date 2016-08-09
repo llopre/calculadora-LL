78e731aa493cb5b34de1650602756e45910bd4df
llopre <llopreiato@hotmail.com>
2016-08-09 03:02:43 +0200

    La final pero mostrando distinto el resultado

--- main.js
+++ main.js
@@ -3,20 +3,35 @@ function Calcular() {
 	var op=document.getElementById('op-selector').value;
 	var valor1=parseInt(document.getElementById('input1').value);
 	var valor2=parseInt(document.getElementById('input2').value);
+	var res; 
 	var result;
+	var tipo;
 
 	switch(op){
 
-		case "S": result = valor1+valor2;
-		break;
-		case "R": result = valor1-valor2;
-		break;
-		case "M": result = valor1*valor2;
-		break;
-		case "D": result = valor1/valor2;
-		break;
+		case "S": 
+				result = valor1+valor2;
+				tipo="+";
+				break;
+		case "R": 
+				result = valor1-valor2;
+				tipo="-";
+				break;
+		case "M": 
+				result = valor1*valor2;
+				tipo="*";
+				break;
+		case "D": 
+				result = valor1/valor2;
+				tipo="/";
+				break;
 
 	}
 
-	alert(result);
+	alert("operacion: " + valor1 + tipo + valor2 + "=" + result);
+	document.setElementById('res').value=res;
+
+
+
+
 }
